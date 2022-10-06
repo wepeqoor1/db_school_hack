5f6e8704d7096dab72b1e6f1c3769207fe9d3c41
Egor Kochergin <46388832+wepeqoor1@users.noreply.github.com>
2022-10-07 01:05:25 +0300

    Convert functions to class

--- scripts.py
+++ scripts.py
@@ -1,36 +1,7 @@
 from random import choice
 from datacenter.models import Lesson, Schoolkid, Mark, Chastisement
 
-
-def fix_marks() -> None:
-    """Исправляет оценки 2 и 3 на 5"""
-    child = Schoolkid.objects.get(full_name__contains=' Фролов Иван Григорьевич')
-    Mark.objects.filter(schoolkid=child).filter(points__in=[2, 3]).update(points=5)
-    Chastisement.objects.filter(schoolkid=child).delete()
-
-    
-def create_commendation(praises: list, child_name: str, lesson: str) -> None:
-    """Добавляет случайные хвалебные коментарии по определенному предмету"""
-    child = Schoolkid.objects.get(full_name__contains=child_name)
-    last_lesson = Lesson.objects.filter(
-        year_of_study=child.year_of_study,
-        group_letter=child.group_letter,
-        subject__title__contains=lesson
-    ).order_by('-date').first()
-
-    praise = choice(praises)
-    chastisement_child = Chastisement.objects.filter(schoolkid=child, subject__title__contains=last_lesson)
-
-    chastisement_child.create(
-        text=praise,
-        created=last_lesson.date,
-        teacher=last_lesson.teacher,
-        schoolkid=child,
-        subject=last_lesson.subject
-    )
-
-
-praises = [
+PRAISES = [
     'Молодец!',
     'Отлично!',
     'Хорошо!',
@@ -62,3 +33,60 @@ praises = [
     'Ты многое сделал, я это вижу!',
     'Теперь у тебя точно все получится!'
 ]
+
+
+class SchoolChild:
+    def __init__(self, child_name: str):
+        self.child_name = child_name
+
+    def fix_marks(self) -> None:
+        """
+        Исправляет оценки 2 и 3 на 5
+        """
+        child = Schoolkid.objects.get(full_name__contains=self.child_name)
+        Mark.objects.filter(schoolkid=child).filter(points__in=[2, 3]).update(
+            points=5)
+
+    def delete_chastisements(self) -> None:
+        """
+        Удаляет плохие замечания
+        """
+        Chastisement.objects.filter(schoolkid=self.child_name).delete()
+
+    def create_commendation(self, lesson: str) -> None:
+        """
+        Добавляет случайные хвалебные коментарии по определенному предмету
+        """
+        child = Schoolkid.objects.get(full_name__contains=self.child_name)
+        last_lesson = Lesson.objects.filter(
+            year_of_study=child.year_of_study,
+            group_letter=child.group_letter,
+            subject__title__contains=lesson
+        ).order_by('-date').first()
+
+        praise = choice(PRAISES)
+        chastisement_child = Chastisement.objects.filter(
+            schoolkid=child,
+            subject__title__contains=last_lesson
+        )
+
+        chastisement_child.create(
+            text=praise,
+            created=last_lesson.date,
+            teacher=last_lesson.teacher,
+            schoolkid=child,
+            subject=last_lesson.subject
+        )
+
+
+def main():
+    child_name = 'Фролов Иван Григорьевич'
+    lesson = 'Музыка'
+    school_child = SchoolChild(child_name=child_name)
+    school_child.fix_marks()
+    school_child.create_commendation(lesson)
+    school_child.delete_chastisements()
+
+
+if __name__ == '__main__':
+    main()
